f2dc130882d72e6ae5af2e48a62ec9f68e578ed5
antonio <antoniorodriguezruiz2002@gmail.com>
2024-11-09 12:42:53 +0100

    feat: unencrypted kafka working

--- src/config/index.js
+++ src/config/index.js
@@ -1,20 +1,19 @@
-import mongoose from 'mongoose';
+import { KafkaClient, Consumer } from 'kafka-node';
 import api from '../api.js';
 
-const MONGO_URI = process.env.MONGOURL;
+const client = new KafkaClient({ kafkaHost: process.env.KAFKA_HOST });
+const consumer = new Consumer(client, [{ topic: 'microservice-logs', partition: 0 }], { autoCommit: true });
 const PORT = process.env.PORT || 3001;
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log('Conexión con MongoDB OK');
+consumer.on('message', (message) => {
+  console.log('Log received:', message.value);
+  // Here, you can save the logs to a database or forward them to a monitoring tool
+});
 
-    const app = api();
+consumer.on('error', (err) => console.error('Error consuming logs', err));
 
-    app.listen(PORT, () => {
-      console.log(`Servidor escuchando en http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error de conexión con MongoDB:', error.message);
-  });
+const app = api();
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});

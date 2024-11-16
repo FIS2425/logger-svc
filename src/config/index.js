dd447df328559549a33f838108a83e12005e9391
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-11-16 20:24:08 +0100

    fix: added automatic topic creation

--- src/config/index.js
+++ src/config/index.js
@@ -2,18 +2,34 @@ import { KafkaClient, Consumer } from 'kafka-node';
 import api from '../api.js';
 
 const client = new KafkaClient({ kafkaHost: process.env.KAFKA_HOST });
-const consumer = new Consumer(client, [{ topic: 'microservice-logs', partition: 0 }], { autoCommit: true });
-const PORT = process.env.PORT || 3001;
 
-consumer.on('message', (message) => {
-  console.log('Log received:', message.value);
-  // Here, you can save the logs to a database or forward them to a monitoring tool
-});
+client.createTopics(['microservice-logs', 'gateway-logs'], (err, _) => {
+  if (err) {
+    console.error('Error creating topic:', err);
+  }
+  console.log(_);
+  const microserviceConsumer = new Consumer(client, [{ topic: 'microservice-logs', partition: 0 }], { autoCommit: true });
+  const gatewayConsumer = new Consumer(client, [{ topic: 'gateway-logs', partition: 0 }], { autoCommit: true });
 
-consumer.on('error', (err) => console.error('Error consuming logs', err));
+  microserviceConsumer.on('message', (message) => {
+    console.log('Log received:', message.value);
+    // Here, you can save the logs to a database or forward them to a monitoring tool
+  });
 
-const app = api();
+  microserviceConsumer.on('error', (err) => console.error('Error consuming logs', err));
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  gatewayConsumer.on('message', (message) => {
+    console.log('Log received:', message.value);
+    // Here, you can save the logs to a database or forward them to a monitoring tool
+  });
+
+  gatewayConsumer.on('error', (err) => console.error('Error consuming logs', err));
+
+  const PORT = process.env.PORT || 3001;
+
+  const app = api();
+
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  });
 });
